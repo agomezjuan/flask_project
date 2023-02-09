5a93855259d0ed96d881b34fe0c6cb1f6b49cea8
Antonio <anavarro67@hotmail.com>
2023-02-09 11:09:10 +0100

    Rama Antonio

--- hello.py
+++ hello.py
@@ -1,35 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine, text
+import pymysql
 
 
 app = Flask(__name__)
 
+conn = pymysql.connect(
+     host="localhost", port=3306, user="root",
+     passwd="", db="pyalmacen"
+ )
 
-engine = create_engine('sqlite:///test.sqlite', echo=True)
-# engine = create_engine("mysql+pymysql://test:test@localhost:3306/test?charset=utf8mb4")
+cursor = conn.cursor()
+cursor.execute(
+    "SELECT nombre, apellido FROM clientes"
+)
 
-with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world'"))
-    print(result.all())
 
-with engine.connect() as conn:
-    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
-    )
-    conn.commit()
-
-with engine.begin() as conn:
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
-    )
-
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT x, y FROM some_table"))
-    for row in result:
-        print(f"x: {row.x}  y: {row.y}")
+for nombre, apellido in cursor.fetchall():
+    print("{0} {1}".format(nombre, apellido))
 
 
 @ app.route('/')
